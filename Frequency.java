35ef01c5fa49f6604d508a5a6f903190d61fd5aa
Manu Emmanuel <manuemmanuel2026@cs.sjcetpalai.ac.in>
2023-10-03 16:02:12 +0530

    JavaLabs

--- Frequency.java
+++ Frequency.java
@@ -3,8 +3,26 @@ public class Frequency {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter a string: ");
-		String word = scan.nextLine();
+		String word = new String();
+		word = scan.nextLine();
+		word = word.toLowerCase();
+		char [] wordTwo = word.toCharArray();
 		System.out.println("Enter the letter to check frequency: ");
-		char letter = scan
+		String letter = scan.next();
+		letter = letter.toLowerCase();
+		char [] letterTwo = letter.toCharArray();
+		int len = word.length();
+		int value = frequency(letterTwo, wordTwo,len);
+		System.out.println("The frequency count is "+value);
 	}
+	
+static int frequency(char [] letterTwo, char [] word, int len) {
+	int count = 0;
+	for (int i = 0; i < len;i ++) {
+		if (word[i] == letterTwo[0]) {
+			count ++;
+		}
+	}
+	return count;
+}
 }
