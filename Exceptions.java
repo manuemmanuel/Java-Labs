710c669c3173ed99c57524dd0f0d739d44239409
Manu Emmanuel <manuemmanuel2026@cs.sjcetpalai.ac.in>
2023-11-10 14:26:29 +0530

    Exceptions

---
+++ Exceptions.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class Exceptions {
+	public static void main(String[] args) {
+		Division();
+
+	}
+	public static void Division() {
+		Scanner scan = new Scanner(System.in);
+		int result = 0;
+		while (true) {
+			System.out.println("Enter the number-1: ");
+			int one = scan.nextInt();
+			System.out.println("Enter the number-2: ");
+			int two = scan.nextInt();
+			try {
+				result = one / two;
+			}
+			catch (ArithmeticException e) {
+				System.out.println("/ by zero");
+			}
+			if (two != 0) {
+				System.out.println("Result: "+result);
+			}
+			System.out.println("End of Operation.");
+			System.out.println("Do you want to continue (y/n): ");
+			String choice = scan.next();
+			char [] answer = choice.toCharArray();
+			if (answer[0] == 'n') {
+				break;
+			}
+				
+			
+		}
+		
+	}
+
+}
