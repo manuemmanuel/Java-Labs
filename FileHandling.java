5adeed8f4769ab9998f9acb4fc9c955cac6b1de0
Manu Emmanuel <manuemmanuel2026@cs.sjcetpalai.ac.in>
2023-11-24 15:44:27 +0530

    Update FileHandling.java

--- FileHandling.java
+++ FileHandling.java
@@ -5,23 +5,20 @@ import java.io.IOException;
 public class FileHandling {
 	public static void main(String[] args) {
 		try {
+			System.out.println("FileWriting");
 			FileWriter fr = new FileWriter("/home/csea2/Manu/Lab/JavaLab/result.txt");
 			String data = new String();
 			data = "Hello";
 			fr.write(data);
 			fr.close();
 			System.out.println("The file is written successfully");
-		} catch (IOException e) {
-			System.out.println("An IO Exception has occured.");
-			e.printStackTrace();
-		}
-		try {
-			FileReader fr = new FileReader("/home/csea2/Manu/Lab/JavaLab/result.txt");
+			System.out.println("FileReading");
+			FileReader file = new FileReader("/home/csea2/Manu/Lab/JavaLab/result.txt");
 			char [] filedata = new char [50];
-			fr.read(filedata);
+			file.read(filedata);
 			System.out.println("The file contains: ");
 			System.out.println(filedata);
-			fr.close();
+			file.close();
 		} catch (FileNotFoundException e) {
 			System.out.println("The File does not exist");
 			e.printStackTrace();
@@ -31,3 +28,4 @@ public class FileHandling {
 		} 
 	}
 }
+
