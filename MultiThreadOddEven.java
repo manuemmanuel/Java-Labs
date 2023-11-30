a1b598b16ac881d7bbb9bcbfd38cd3ca3e89f0b1
Manu Emmanuel <manuemmanuel2026@cs.sjcetpalai.ac.in>
2023-11-30 14:53:49 +0530

    Update MultiThreadOddEven.java

--- MultiThreadOddEven.java
+++ MultiThreadOddEven.java
@@ -1,4 +1,4 @@
-/*Write a java program that creates three threads. First thread generates a random positive number (>1) every second. If the number is even, */
+/*Write a java program that creates three threads. First thread generates a random positive number (>1) every second. If the number is even, the second thread */
 package javaclass;
 import java.util.Random;
 public class MultiThreadOddEven {
@@ -11,6 +11,9 @@ public class MultiThreadOddEven {
 class NumberManager {
 	private int generatedNumber;
 	private boolean numberGenerator = false;
+	public synchronized void generateNumber() {
+		
+	}
 }
 class ThreadOne extends Thread {
 	
