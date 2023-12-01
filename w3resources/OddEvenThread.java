4f927bc905639f68968fc845579d9e28e0cb2561
Manu Emmanuel <manuemmanuel2026@cs.sjcetpalai.ac.in>
2023-12-01 22:43:16 +0530

    Create OddEvenThread.java

---
+++ w3resources/OddEvenThread.java
@@ -0,0 +1,10 @@
+public class EvenOddThreads {
+    public static void main(String[] args){
+
+    }
+}
+class Odd extends Thread {
+    public void run(){
+        for (int i = 0;i < 20; i++)
+    }
+}
