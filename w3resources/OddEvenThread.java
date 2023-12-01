7edb9d1b9b4e972ee9d234b7eef5abf1fe87b783
Manu Emmanuel <manuemmanuel2026@cs.sjcetpalai.ac.in>
2023-12-01 22:47:23 +0530

    Update OddEvenThread.java

--- w3resources/OddEvenThread.java
+++ w3resources/OddEvenThread.java
@@ -5,6 +5,11 @@ public class EvenOddThreads {
 }
 class Odd extends Thread {
     public void run(){
-        for (int i = 0;i < 20; i++)
+        System.out.println("The Odd numbers: ");
+        for (int i = 0;i < 20; i++){
+            if (i % 2 != 0){
+                System.out.println(i);
+            }
+        }
     }
 }
