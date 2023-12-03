08db03fc31a6240082ccaf196669729d4119256d
Manu Emmanuel <manuemmanuel2026@cs.sjcetpalai.ac.in>
2023-12-04 00:33:54 +0530

    Update TriangleAreaCalculator.java

--- TriangleAreaCalculator.java
+++ TriangleAreaCalculator.java
@@ -8,15 +8,10 @@ public class TriangleAreaCalculator {
         double side2 = scanner.nextDouble();
         System.out.print("Input Side-3: ");
         double side3 = scanner.nextDouble();
-
         double area = calculateTriangleArea(side1, side2, side3);
-
         System.out.println("The area of the triangle is " + area);
     }
-
-    // Method to calculate the area of a triangle using Heron's formula
     public static double calculateTriangleArea(double side1, double side2, double side3) {
-        // Calculate the semi-perimeter
         double s = (side1 + side2 + side3) / 2;
 
         // Calculate the area using Heron's formula
