0a6edc002c8ca5b265f2b96d54a86edced50b657
Manu Emmanuel <manuemmanuel2026@cs.sjcetpalai.ac.in>
2023-12-04 00:33:30 +0530

    Update TriangleAreaCalculator.java

--- TriangleAreaCalculator.java
+++ TriangleAreaCalculator.java
@@ -1,16 +1,11 @@
 import java.util.Scanner;
-
 public class TriangleAreaCalculator {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.print("Input Side-1: ");
         double side1 = scanner.nextDouble();
-
         System.out.print("Input Side-2: ");
         double side2 = scanner.nextDouble();
-
         System.out.print("Input Side-3: ");
         double side3 = scanner.nextDouble();
 
