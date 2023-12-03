99f2bd671242e495c6f5a2b2fc0e2d5d288b02a6
Manu Emmanuel <manuemmanuel2026@cs.sjcetpalai.ac.in>
2023-12-04 00:34:11 +0530

    Update TriangleAreaCalculator.java

--- TriangleAreaCalculator.java
+++ TriangleAreaCalculator.java
@@ -13,10 +13,7 @@ public class TriangleAreaCalculator {
     }
     public static double calculateTriangleArea(double side1, double side2, double side3) {
         double s = (side1 + side2 + side3) / 2;
-
-        // Calculate the area using Heron's formula
         double area = Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
-
         return area;
     }
 }
