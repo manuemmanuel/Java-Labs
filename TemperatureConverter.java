f806add460627c39caf170d7c6c626ac349b37df
Manu Emmanuel <manuemmanuel2026@cs.sjcetpalai.ac.in>
2023-12-04 12:16:00 +0530

    Update TemperatureConverter.java

--- TemperatureConverter.java
+++ TemperatureConverter.java
@@ -4,9 +4,11 @@ import java.awt.event.*;
 public class TemperatureConverter extends JFrame implements ActionListener {
 	private JTextField celsiusTextField;
 	private JTextField farenheitTextField;
+	TemperatureConverter(){
+		setTitle("Temperature Converter");
+	}
 	public static void main(String[] args) {
-		
+	
 
 	}
 }
-
