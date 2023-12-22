bad0e481da1a310a669d829b725e154cedf6181e
Manu Emmanuel <manuemmanuel2026@cs.sjcetpalai.ac.in>
2023-12-22 15:22:22 +0530

    update

--- SwingDemo.java
+++ SwingDemo.java
@@ -2,9 +2,9 @@ import javax.swing.*;
 class SwingDemo {
   SwingDemo(){
     JFrame jframe = new JFrame("Swing application");  
-  jframe.setSize(275,100);
-  JLabel jlabel = new JLabel("Hello World");
-  jframe.add(jlabel);
-  jframe.setVisible(true);
+    jframe.setSize(275,100);
+    JLabel jlabel = new JLabel("Hello World");
+    jframe.add(jlabel);
+    jframe.setVisible(true);
   }
 }
